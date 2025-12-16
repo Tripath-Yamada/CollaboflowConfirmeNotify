b17791436cc8f3aeabeb1b5115632bd7c0ccb123
Shoki Yamada <s_yamada@triapth.co.jp>
2025-12-16 09:26:45 +0900

    即時通知バージョンほぼ完成

--- src/index.ts
+++ src/index.ts
@@ -89,43 +89,57 @@ function doPost(e: GoogleAppsScript.Events.DoPost) {
     const approversArray: unknown[] = [].concat(requestObj.determs || []);
 
     sheet.clear();
-    sheet.appendRow([requestObj.action_type]);
-    let header_text = '';
 
-    const text =
-      '申請者: ' +
-      requestObj.request_user.name +
-      '\n\n' +
-      'タイトル: ' +
-      requestObj.document_title +
-      '\n\n' +
-      'URL: ' +
-      'https://cloud.collaboflow.com/tripath/index.cfm?app_cd=1&fuseaction=clz_DocumentDesc&det=no&nPinstanceCD=' +
-      requestObj.document_id;
-
+    const selfConfirmText =
+      `【申請完了通知】\n\n` +
+      `申請が完了しました。\n\n` +
+      `文書タイトル: ${requestObj.document_title} \n` +
+      `申請書URL: https://cloud.collaboflow.com/tripath/index.cfm?app_cd=1&fuseaction=clz_DocumentDesc&det=no&nPinstanceCD=${requestObj.document_id}`;
     // 申請完了時(決済)、本人に通知を送信
     if (
       requestObj.action_type === 'end' &&
       requestObj.flow_status === 'decision'
     ) {
-      header_text = '【申請完了通知】\n\n';
       const env = getEnv_selfConfirm();
-      env.userId = requestObj.request_user.email;
-      LINEWORKS.userMessageSend(env, header_text + text);
-      return;
+      env.userId = convertLineWorksAccountIdFromEmail(
+        requestObj.request_user.email
+      );
+      // 出張申請の場合を除いて(出張申請という文字列からはじまらない)本人に完了通知を送信
+      // 出張申請は別途タスク通知メールで完了通知を送信する
+      if (requestObj.document_title.startsWith('出張申請') === false) {
+        LINEWORKS.userMessageSend(env, selfConfirmText);
+      }
     } else if (requestObj.action_type === 'reject') {
       // 申請却下時、本人に通知を送信
-      header_text =
-        '【申請却下通知】\n\n' + '申請が却下されました。確認してください。\n\n';
+      const rejectText =
+        `【申請却下通知】\n\n` +
+        `申請が却下されました。確認してください。\n\n` +
+        `文書タイトル: ${requestObj.document_title} \n` +
+        `申請書URL: https://cloud.collaboflow.com/tripath/index.cfm?app_cd=1&fuseaction=clz_DocumentDesc&det=no&nPinstanceCD=${requestObj.document_id}`;
       const env = getEnv_selfConfirm();
-      env.userId = requestObj.request_user.email;
-      LINEWORKS.userMessageSend(env, header_text + text);
+      env.userId = convertLineWorksAccountIdFromEmail(
+        requestObj.request_user.email
+      );
+      LINEWORKS.userMessageSend(env, rejectText);
       return;
     } else if (requestObj.action_type === 'confirm') {
       // confirm (確認時)のアクションタイプは処理しない
       return;
     } else if (requestObj.action_type === 'accept') {
       // accept (承認時)のアクションタイプは判定者に通知を送信しない
+      return;
+    } else if (requestObj.action_type === 'remand') {
+      // remand (差戻し時)のアクションタイプは本人(本人にしか差し戻せない設定にする)に通知を送信
+      const rejectText =
+        `【申請差し戻し通知】\n\n` +
+        `申請が差し戻されました。確認してください。\n\n` +
+        `文書タイトル: ${requestObj.document_title} \n` +
+        `申請書URL: https://cloud.collaboflow.com/tripath/index.cfm?app_cd=1&fuseaction=clz_DocumentDesc&det=no&nPinstanceCD=${requestObj.document_id}`;
+      const env = getEnv_selfConfirm();
+      env.userId = convertLineWorksAccountIdFromEmail(
+        requestObj.request_user.email
+      );
+      LINEWORKS.userMessageSend(env, rejectText);
       return;
     } else {
       // その他のアクションタイプ（承認要求、確認待ちなど）は判定者に通知を送信
@@ -152,16 +166,31 @@ function doPost(e: GoogleAppsScript.Events.DoPost) {
           sheet.appendRow([determ_status]);
           // 判定ステータスが確認待ちの場合のみ通知
           if (determ_status === 'unconfirm') {
-            header_text = name + 'さん【確認通知】\n\n';
+            const confirmText =
+              `${name}さん【確認通知】\n\n` +
+              `あなたの確認が必要な申請があります。\n\n` +
+              `申請者: ${requestObj.request_user.name} \n` +
+              `申請者部署: ${requestObj.request_group.name} \n` +
+              `文書タイトル: ${requestObj.document_title} \n` +
+              `文書番号: ${requestObj.document_number} \n` +
+              `申請書URL: https://cloud.collaboflow.com/tripath/index.cfm?app_cd=1&fuseaction=clz_DocumentDesc&det=no&nPinstanceCD=${requestObj.document_id}`;
             const env = getEnv_confirm();
-            env.userId = email;
-            LINEWORKS.userMessageSend(env, header_text + text);
+            env.userId = convertLineWorksAccountIdFromEmail(email);
+            LINEWORKS.userMessageSend(env, confirmText);
           }
-          if (determ_status !== 'unconfirm') {
-            header_text = name + 'さん【承認要求通知】\n\n';
+          // 判定ステータスが未承認の場合
+          else if (determ_status === 'undeterm') {
+            const determText =
+              `${name}さん【承認要求通知】\n\n` +
+              `あなたの承認が必要な申請があります。\n\n` +
+              `申請者: ${requestObj.request_user.name} \n` +
+              `申請者部署: ${requestObj.request_group.name} \n` +
+              `文書タイトル: ${requestObj.document_title} \n` +
+              `文書番号: ${requestObj.document_number} \n` +
+              `申請書URL: https://cloud.collaboflow.com/tripath/index.cfm?app_cd=1&fuseaction=clz_DocumentDesc&det=no&nPinstanceCD=${requestObj.document_id}`;
             const env = getEnv_approve();
-            env.userId = email;
-            LINEWORKS.userMessageSend(env, header_text + text);
+            env.userId = convertLineWorksAccountIdFromEmail(email);
+            LINEWORKS.userMessageSend(env, determText);
           }
         }
       }
@@ -177,7 +206,7 @@ function doPost(e: GoogleAppsScript.Events.DoPost) {
   } catch (error) {
     const env = getEnv_confirm();
     env.userId = 'yamadashoki@tripath-trial';
-    LINEWORKS.userMessageSend(env, 'error: ' + String(error));
+    LINEWORKS.userMessageSend(env, `error: ${String(error)}`);
   }
 }
 
@@ -187,30 +216,20 @@ function createBot() {
   const env = getEnv_base();
   env.BOT_ID = LINEWORKS.qiitaSampleBotCreate(env).botId;
   LINEWORKS.qiitaSampleBotDomainRegister(env);
-  Logger.log('BOT_ID: ' + String(env.BOT_ID));
+  Logger.log(`BOT_ID: ${String(env.BOT_ID)}`);
 }
 
 // Gmailをチェックして、特定の条件に合致するメールがあればLINEWORKSのWebhookに通知を送信する関数(主に管理部確認用)
 // トリガーで定期実行することを想定
 function checkGmailAndNotifyWebhook() {
   const keyword = 'コラボフロー確認通知'; // 件名に含まれるキーワード（空の場合は無視）
+  const keywordTask = '[コラボフロー]　申請依頼が届きました'; // 件名に含まれるキーワード（空の場合は無視）
+  const keywordKanri = 'コラボフロー管理部出張申請確認通知'; // 件名に含まれるキーワード（空の場合は無視）
   const senderEmail = 'tripath@collaboflow.com'; // 特定の送信者のメールアドレス（空の場合は無視）
   const numThreadsToProcess = 20; // 処理するスレッド数を指定
-  const webhookUrl =
-    'https://webhook.worksmobile.com/message/ed01d020-c8c7-44fe-bf85-1b5db0114df5';
   const lastProcessedDateKey = 'LAST_PROCESSED_DATE';
-  const kanribuId = 'kanri@tripath-trial'; // 管理部アカウント(全体通知用ダミーアカウント)
-  const kanribuChannelId = 'fec3b462-0c3f-b275-5c69-b0c4b83cb956'; // 管理部LINEWORKS確認通知用チャンネルID
-
-  // 日付をフォーマットする関数
-  function formatDate(date: Date) {
-    const yyyy = date.getFullYear();
-    const mm = ('0' + (date.getMonth() + 1)).slice(-2);
-    const dd = ('0' + date.getDate()).slice(-2);
-    const HH = ('0' + date.getHours()).slice(-2);
-    const MM = ('0' + date.getMinutes()).slice(-2);
-    return `${yyyy} 年 ${mm} 月 ${dd} 日 ${HH} 時 ${MM} 分`;
-  }
+  // const kanribuId = 'kanri@tripath-trial'; // 管理部アカウント(全体通知用ダミーアカウント)
+  // const kanribuChannelId = 'fec3b462-0c3f-b275-5c69-b0c4b83cb956'; // 管理部LINEWORKS確認通知用チャンネルID
 
   // 前回の処理日時を取得
   const properties = PropertiesService.getScriptProperties();
@@ -220,12 +239,12 @@ function checkGmailAndNotifyWebhook() {
     : 0;
 
   Logger.log(
-    '開始: メールのチェックを開始します。前回の処理日時: ' + lastProcessedDate
+    `開始: メールのチェックを開始します。前回の処理日時: ${lastProcessedDate}`
   );
 
   try {
     const threads = GmailApp.getInboxThreads(0, numThreadsToProcess); // 指定した数のスレッドを取得
-    Logger.log('取得したスレッド数: ' + threads.length);
+    Logger.log(`取得したスレッド数: ${threads.length}`);
 
     let newLastProcessedTimestamp = lastProcessedTimestamp; // 新しい最後の処理日時を初期化
 
@@ -240,58 +259,128 @@ function checkGmailAndNotifyWebhook() {
 
         // メッセージが前回の処理日時以降であるかチェック
         if (messageDate > lastProcessedTimestamp) {
+          const address = message.getTo();
           const subject = message.getSubject();
           const from = message.getFrom();
           const body = message.getPlainBody();
-          const formattedDate = formatDate(new Date(messageDate));
 
-          // 条件をチェック
+          // 確認通知条件をチェック
           const keywordMatch = !keyword || subject.includes(keyword);
           const senderMatch = !senderEmail || from.includes(senderEmail);
 
           if (keywordMatch && senderMatch) {
             Logger.log(
-              '条件に一致するメールを発見: 件名: ' +
-                subject +
-                ', 送信者: ' +
-                from
+              `条件に一致するメールを発見: 件名: ${subject}, 送信者: ${from}, 宛先: ${address}`
             );
 
             try {
               // bodyをJSONに変換する場合は、以下のようにする
               const bodyJson = JSON.parse(body);
-              bodyJson.contacts.forEach((contact: string) => {
-                const env = getEnv_confirm();
-                const text =
-                  '申請の管理部全体向けの確認通知を受信しました。' +
-                  '\n\n' +
-                  '申請者: ' +
-                  bodyJson.who +
-                  '\n\n' +
-                  '受信日時: ' +
-                  formattedDate +
-                  '\n\n' +
-                  'タイトル: ' +
-                  bodyJson.title +
-                  '\n\n' +
-                  'URL: ' +
-                  bodyJson.url;
-                if (contact === kanribuId) {
-                  // 通知先のアカウントが管理部アカウントの場合は、チャンネルIDを設定してメッセージを送信
-                  env.channelId = kanribuChannelId;
-                  LINEWORKS.channelMessageSend(env, text);
-                } else {
-                  // 管理部アカウントではない場合は、ユーザーIDを設定してメッセージを送信
-                  env.userId = contact;
-                  LINEWORKS.userMessageSend(env, text);
-                }
-              });
+              const env = getEnv_confirm();
+              const text =
+                `${address} \n\n` +
+                `【確認通知】 \n\n` +
+                `下記、申請の確認通知が届きました。確認をお願いいたします。\n\n` +
+                `申請者: ${bodyJson.who}\n` +
+                `申請者部署: ${bodyJson.busho}\n` +
+                `文書タイトル: ${bodyJson.title}\n` +
+                `文書番号: ${bodyJson.requestno}\n` +
+                `申請書URL: ${bodyJson.url}`;
+              env.userId = convertLineWorksAccountIdFromEmail(address);
+              LINEWORKS.userMessageSend(env, text);
+              Logger.log(
+                `LINEWORKSのメッセージ送信に成功しました: 件名: ${subject}`
+              );
             } catch (e) {
               Logger.log(
-                'LINEWORKSのメッセージ送信が失敗しました: 件名: ' +
-                  subject +
-                  ', エラー: ' +
-                  String(e)
+                `LINEWORKSのメッセージ送信が失敗しました: 件名: ${subject}, エラー: ${String(e)}`
+              );
+            }
+          }
+
+          // タスク通知条件をチェック
+          const keywordMatchTask =
+            !keywordTask || subject.includes(keywordTask);
+          const senderMatchTask = !senderEmail || from.includes(senderEmail);
+
+          if (keywordMatchTask && senderMatchTask) {
+            Logger.log(
+              `条件に一致するタスク通知メールを発見: 件名: ${subject}, 送信者: ${from}, 宛先: ${address} \n\n`
+            );
+
+            try {
+              // bodyの「--以下はシステムで利用します。」以下、「下記のURLをクリックして、依頼内容を確認して下さい。」以上を抽出してJSONに変換する
+              const bodyLines = body.split('\n');
+              let jsonString = '';
+              let isJsonPart = false;
+              for (const line of bodyLines) {
+                if (line.includes('--以下はシステムで利用します。')) {
+                  isJsonPart = true;
+                  continue;
+                }
+                if (
+                  line.includes(
+                    '下記のURLをクリックして、依頼内容を確認して下さい。'
+                  )
+                ) {
+                  break;
+                }
+                if (isJsonPart) {
+                  jsonString += line + '\n';
+                }
+              }
+              const bodyJson = JSON.parse(jsonString);
+              const env = getEnv_selfConfirm();
+              const text =
+                `${address} \n\n` +
+                `【タスク通知】 \n\n` +
+                `下記、申請が登録され、タスクが作成されました。タスク一覧を確認し、申請書を提出してください。\n\n` +
+                `文書タイトル: ${bodyJson.title}\n` +
+                `文書番号: ${bodyJson.requestno}\n` +
+                `タスク一覧URL: https://cloud.collaboflow.com/tripath/index.cfm?app_cd=1&fuseaction=clz&folderid=task`;
+              env.userId = convertLineWorksAccountIdFromEmail(address);
+              LINEWORKS.userMessageSend(env, text);
+              Logger.log(
+                `LINEWORKSのタスクメッセージ送信に成功しました: 件名: ${subject}`
+              );
+            } catch (e) {
+              Logger.log(
+                `LINEWORKSのタスクメッセージ送信が失敗しました: 件名: ${subject}, エラー: ${String(e)}`
+              );
+            }
+          }
+
+          // 確認通知条件をチェック
+          const keywordKanriMatch =
+            !keywordKanri || subject.includes(keywordKanri);
+          const senderKanriMatch = !senderEmail || from.includes(senderEmail);
+
+          if (keywordKanriMatch && senderKanriMatch) {
+            Logger.log(
+              `条件に一致するメールを発見: 件名: ${subject}, 送信者: ${from}, 宛先: ${address}`
+            );
+
+            try {
+              // bodyをJSONに変換する場合は、以下のようにする
+              const bodyJson = JSON.parse(body);
+              const env = getEnv_confirm();
+              const text =
+                `${address} \n\n` +
+                `【管理部出張申請確認通知】 \n\n` +
+                `下記出張申請の出張報告が完了しました。 出張報告完了確認をお願いいたします。\n\n` +
+                `申請者: ${bodyJson.who}\n` +
+                `申請者部署: ${bodyJson.busho}\n` +
+                `出張申請書文書タイトル: ${bodyJson.title}\n` +
+                `出張申請書文書番号: ${bodyJson.requestno}\n` +
+                `出張申請書URL: ${bodyJson.url}`;
+              env.userId = convertLineWorksAccountIdFromEmail(address);
+              LINEWORKS.userMessageSend(env, text);
+              Logger.log(
+                `LINEWORKSのメッセージ送信に成功しました: 件名: ${subject}`
+              );
+            } catch (e) {
+              Logger.log(
+                `LINEWORKSのメッセージ送信が失敗しました: 件名: ${subject}, エラー: ${String(e)}`
               );
             }
           }
@@ -310,12 +399,16 @@ function checkGmailAndNotifyWebhook() {
       new Date(newLastProcessedTimestamp).toISOString()
     );
     Logger.log(
-      '完了: メールのチェックを完了しました。新しい最後の処理日時: ' +
-        new Date(newLastProcessedTimestamp).toISOString()
+      `完了: メールのチェックを完了しました。新しい最後の処理日時: ${new Date(newLastProcessedTimestamp).toISOString()}`
     );
   } catch (e) {
     Logger.log(
-      'メールの取得中にエラーが発生しました: ' + (e as Error).toString()
+      `メールの取得中にエラーが発生しました: ${(e as Error).toString()}`
     );
   }
+}
+
+// admin+xxxx@bankin-labo.comメールのエイリアスからLINEWORKSのアカウントIDに変換する関数
+function convertLineWorksAccountIdFromEmail(email: string) {
+  return `${email.split('admin+')[1].split('@bankin-labo.com')[0]}@tripath-trial`;
 }
